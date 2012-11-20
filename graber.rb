90016c73ae80d02b63faf685eec99edcd2c54ab7
«Jeaninne» <bokhan.e@gmail.com>
2012-11-20 22:08:28 +0200

    start

---
+++ graber.rb
@@ -0,0 +1,21 @@
+require 'net/http'
+
+class Link_writer 
+
+ def write_in_file
+  #write array in file
+ end
+
+ def find_links
+ end
+
+ def find_links(page_name, depth)
+  #go to the link
+  #call method find_links and write result 
+  #if depth > 1 then
+  #foreach
+  #call find_links(page_name, depth-1)
+  #esle return array of links
+ end
+
+end
