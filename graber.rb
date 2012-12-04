55cf1ce8678ab040dd0be9b726a342c78d240b69
«Jeaninne» <bokhan.e@gmail.com>
2012-12-04 21:56:38 +0200

    worked for depth=1 and ful adresses

--- graber.rb
+++ graber.rb
@@ -3,33 +3,35 @@ require 'nokogiri'
 require 'open-uri'
 
 class Link_graber
- attr_accessor :link 
+ #attr_accessor :link 
 
- def initialize(link)
- @link= Nokogiri::HTML(open(link))
+ def initialize#(link)
+ #@link= Nokogiri::HTML(open(link))
  end
 
- def write_in_file
-  #write array in file
+ def write_in_file(string)
+  File.open('data.txt', 'a'){ |file| file.puts  string }
  end
 
  def parse(page_name)
+  links = []
   doc = Nokogiri::HTML(open(page_name))
-  doc.xpath('//a').each do |link|
-  puts link[:href]
+   doc.xpath('//a').each do |link|
+     links << link[:href] if link[:href].include?('http') 
   end
+  links
  end
 
  def find_links(page_name, depth)
-  #go to the link
-  #call method find_links and write result 
-  #if depth > 1 then
-  #foreach
-  #call find_links(page_name, depth-1)
-  #esle return array of links
+  links_array = parse(page_name)
+  if depth > 1 then 
+   links_array.each {|page| find_links(page, depth-1)
+    write_in_file(page)}
+  else links_array.each {|page| write_in_file(page)}
+   end
  end
 
 end
 
-x = Link_graber.new('http://kote.dp.ua/')
-p x.parse('http://kote.dp.ua/')
+x = Link_graber.new
+x.find_links('http://www.kote.dp.ua', 1)
